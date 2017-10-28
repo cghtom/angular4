c3bffed31068bf5e99d3c5979159482806c48fce
ruben056 <demuynck_ruben@hotmail.com>
2017-10-28 08:02:00 +0200

    adding form validation using * provided validators (required etc.. * custom validators

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,25 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   myForm:FormGroup;
+  productName:string;
 
   constructor(fb: FormBuilder){
     this.myForm = fb.group({
-      'sku': ['ABC123']
+      'sku': ['', Validators.compose(
+        [
+          Validators.required,
+          skuValidator
+        ])],
+        'productName':['', Validators.required]
+    });
+    this.myForm.controls['sku'].valueChanges.subscribe( (value:string) =>{
+      console.log("sku changed to " + value);
+    });
+    this.myForm.valueChanges.subscribe( (value:any) =>{
+      console.log("form changed to " + value);
+      this.logFormValidationErrors();
     });
   }
 
@@ -20,5 +34,21 @@ export class AppComponent {
     console.log("you submitted :", formValues);
   }
 
-
+  logFormValidationErrors():void{
+    Object.keys(this.myForm.controls).forEach(key => {
+    const controlErrors: ValidationErrors = this.myForm.get(key).errors;
+    if (controlErrors != null) {
+          Object.keys(controlErrors).forEach(keyError => {
+            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+          });
+        }
+      });
+  }
 }
+
+
+function skuValidator(c:FormControl):{[s: string]:boolean}{
+  if(!c.value.match(/^123/)){
+    return  {mustStartWith123: true};
+  }
+};

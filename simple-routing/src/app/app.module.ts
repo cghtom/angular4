265b2e856b89ff325924d204abfeffc2709db1d1
ruben056 <demuynck_ruben@hotmail.com>
2017-10-29 10:45:46 +0100

    Implemented basic routing using hashlocationstrategy, routes, routerlink, routeroutlet, ... This allows user to bookmark and back and forth in history with the browser buttons

--- simple-routing/src/app/app.module.ts
+++ simple-routing/src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+
+const routes:Routes = [
+  {path : '', redirectTo : 'home', pathMatch : 'full'},
+  {path : 'home', component : HomeComponent},
+  {path : 'about', component : AboutComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'contactus', redirectTo : 'contact'}
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }, // by default angular4 uses html5 shizzle that is not supported by all browsers yet ... hashlocationstrategy always works...
+    { provide: APP_BASE_HREF, useValue: '/' } // used for relative routes
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

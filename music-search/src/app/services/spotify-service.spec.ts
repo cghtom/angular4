5ffd97aa8160233c157db1e389553dd00a231956
ruben056 <demuynck_ruben@hotmail.com>
2017-12-09 12:26:30 +0100

    * Fixed missing brackets (compile error was off course correct after all :) ) * Added unit test for searchTrack and getAlbum

--- music-search/src/app/services/spotify-service.spec.ts
+++ music-search/src/app/services/spotify-service.spec.ts
@@ -4,6 +4,14 @@ import { Http, ConnectionBackend, Response, ResponseOptions, BaseRequestOptions 
 
 import { SpotifyService } from './spotify-service';
 
+function setupExpectedUrlAndMockResponse(mockBackend: MockBackend, urlToExpect:string, responseBody:string){
+  mockBackend.connections.subscribe(c => {
+    expect(c.request.url).toBe(urlToExpect);
+    let response = new ResponseOptions({ body: responseBody });
+    c.mockRespond(new Response(response));
+  });
+}
+
 describe('SpotifyService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,23 +30,58 @@ describe('SpotifyService', () => {
     });
   });
 
-describe('getTrack', () => {
+  describe('getTrack call rest api with trackid', () => {
     it('retrieves using the track ID',
       inject([SpotifyService, MockBackend],
-          fakeAsync((spotifyService, mockBackend) => {
-            mockBackend.connections.subscribe(c => {
-              expect(c.request.url).toBe('https://api.spotify.com/v1/tracks/TRACK_ID');
-              let response = new ResponseOptions({ body: '{"name": "felipe"}' });
-              c.mockRespond(new Response(response));
-            });
+        fakeAsync((spotifyService, mockBackend) => {
 
-            let res;
-            spotifyService.getTrack('TRACK_ID').subscribe((_res) => {
-              res = _res;
-            });
-            tick();
-            expect(res.name).toBe('felipe');
-          })
+          setupExpectedUrlAndMockResponse(mockBackend, 'https://api.spotify.com/v1/tracks/TRACK_ID', '{"name": "felipe"}');
+
+          let res;
+          spotifyService.getTrack('TRACK_ID').subscribe((_res) => {
+            res = _res;
+          });
+          tick();
+          expect(res.name).toBe('felipe');
+        })
       )
     );
+    it('Retrieves album by calling correct rest api',
+      inject([SpotifyService, MockBackend],
+        fakeAsync((spotifyService, mockBackend) => {
+          // 1. validate outgoing call to rest api + mock a response
+          setupExpectedUrlAndMockResponse(mockBackend, 'https://api.spotify.com/v1/albums/ALBUM_ID', '{"name" : "riders on the storm"}');
+
+          // 2. trigger operation to test
+          let actual;
+          spotifyService.getAlbum('ALBUM_ID').subscribe((response) => {
+            actual = response;
+          });
+          tick(); //--> 2.1 make async call happen
+
+          // 3. assert final result
+          expect(actual.name).toBe('riders on the storm');
+        })
+      )
+    );
+
+    it('Search tracks by calling correct rest api',
+      inject([SpotifyService, MockBackend],
+        fakeAsync((spotifyService, mockBackend) => {
+          // 1. validate outgoing call to rest api + mock a response
+          setupExpectedUrlAndMockResponse(mockBackend, 'https://api.spotify.com/v1/search?q=TRACK_QRY&type=track', '{"name" : "Hello, I love you"}');
+
+          // 2. trigger operation to test
+          let actual;
+          spotifyService.searchTrack('TRACK_QRY').subscribe((response) => {
+            actual = response;
+          });
+          tick(); //--> 2.1 make async call happen
+
+          // 3. assert final result
+          expect(actual.name).toBe("Hello, I love you");
+        })
+      )
+    );
+  });
 });
